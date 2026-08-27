0a061d42e5d77747173b051a6f20e7e60b5d25c1
UnplannedCoder <pawansa2006@gmail.com>
2026-08-27 22:30:30 +0530

    Adding files

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit'
-import codeNestReducer from './redux/Slice'
-import authReducer from './redux/authSlice'
+import { configureStore } from "@reduxjs/toolkit";
+import codeNestReducer from "./redux/Slice";
+import authReducer from "./redux/authSlice";
 
 export const store = configureStore({
   reducer: {
     codenest: codeNestReducer,
     auth: authReducer,
   },
-})
+});
