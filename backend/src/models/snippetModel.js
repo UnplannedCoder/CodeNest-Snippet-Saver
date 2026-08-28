d3d377fe44837a10943e3b333d58b07bda39958e
UnplannedCoder <pawansa2006@gmail.com>
2026-08-28 17:33:52 +0530

    Adding files

--- backend/src/models/snippetModel.js
+++ backend/src/models/snippetModel.js
@@ -1,29 +1,29 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const snippetSchema = new mongoose.Schema(
   {
     user: {
       type: mongoose.Schema.Types.ObjectId,
       required: true,
-      ref: 'User',
+      ref: "User",
     },
     title: {
       type: String,
-      required: [true, 'Please add a snippet title'],
+      required: [true, "Please add a snippet title"],
       trim: true,
     },
     content: {
       type: String,
-      required: [true, 'Please add snippet content'],
+      required: [true, "Please add snippet content"],
     },
   },
   {
     timestamps: true,
-  }
+  },
 );
 
 snippetSchema.index({ user: 1, createdAt: -1 });
 
-const Snippet = mongoose.model('Snippet', snippetSchema);
+const Snippet = mongoose.model("Snippet", snippetSchema);
 
 export default Snippet;
