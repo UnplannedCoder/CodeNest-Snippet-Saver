1fe8099727b85ed213b077f2c76544cb730185d9
UnplannedCoder <pawansa2006@gmail.com>
2026-08-28 17:34:32 +0530

    Adding files

--- backend/src/routes/snippetRoutes.js
+++ backend/src/routes/snippetRoutes.js
@@ -1,23 +1,22 @@
-import express from 'express';
+import express from "express";
 import {
   getSnippets,
   getSnippetById,
   createSnippet,
   updateSnippet,
   deleteSnippet,
-} from '../controllers/snippetController.js';
-import { protect } from '../middleware/authMiddleware.js';
+} from "../controllers/snippetController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 // All snippet routes require JWT authentication
 router.use(protect);
 
-router.route('/')
-  .get(getSnippets)
-  .post(createSnippet);
+router.route("/").get(getSnippets).post(createSnippet);
 
-router.route('/:id')
+router
+  .route("/:id")
   .get(getSnippetById)
   .put(updateSnippet)
   .delete(deleteSnippet);
