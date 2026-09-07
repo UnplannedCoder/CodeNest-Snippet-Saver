9b9ff0aa21a2679ca25f9f10d30e96dae7e0cea5
UnplannedCoder <pawansa2006@gmail.com>
2026-09-07 13:10:29 +0530

    adding files

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,12 @@
-import express from "express"
-import dotenv from "dotenv"
-import cors from "cors"
-import connectDB from "./config/db.js"
-import authRoutes from "./routes/authRoutes.js"
-import snippetRoutes from "./routes/snippetRoutes.js"
-import { notFound, errorHandler } from "./middleware/errorMiddleware.js"
-import path from "path"
-import { fileURLToPath } from "url"
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+import snippetRoutes from "./routes/snippetRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
